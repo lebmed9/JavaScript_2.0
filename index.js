65ca084b2698284ae892b851d3f59873e6938a2c
Andrei <lebmed9@gmail.com>
2023-01-21 07:29:18 -0600

    Lesson_7_closure

--- index.js
+++ index.js
@@ -43,36 +43,97 @@
 // let sayBound2 = say.bind(user1, "Привет");
 // sayBound2();
 
-// Practice\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\start///////////////////////////////
+/////////////////////////////////////////////Practice_start/////////////////////////////////////////////
 
-const city = {
-  name: "NN",
-  population: 2000000,
-  showName: function () {
-    console.log(this.name);
-  },
-  showPopulation() {
-    console.log(this.population);
-  },
-  setName(name) {
-    this.name = name;
-  },
-};
+// const city = {
+//   name: "NN",
+//   population: 2000000,
+//   showName: function () {
+//     console.log(this.name);
+//   },
+//   showPopulation() {
+//     console.log(this.population);
+//   },
+//   setName(name) {
+//     this.name = name;
+//   },
+// };
 
-const city2 = {
-  name: "San Jose",
-  population: 1500000,
-};
+// const city2 = {
+//   name: "San Jose",
+//   population: 1500000,
+// };
 
-// city.setName("San Jose");
-// city.showName();
-city.showPopulation.call(city2);
-let test = city.showPopulation.bind(city2);
-let test2 = city.setName.bind(city2, "Santa Ana");
-test2();
-console.log(city2);
-test2("Moscow");
-console.log(city2);
+// // city.setName("San Jose");
+// // city.showName();
+// city.showPopulation.call(city2);
+// let test = city.showPopulation.bind(city2);
+// let test2 = city.setName.bind(city2, "Santa Ana");
+// test2();
+// console.log(city2);
+// test2("Moscow");
+// console.log(city2);
 
-test();
-// Practice\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\end///////////////////////////////
+// test();
+
+/////////////////////////////////////////////Practice_end/////////////////////////////////////////////
+
+// function example() {
+//   let closure = 100;
+//   return function (b) {
+//     return closure + b;
+//   };
+// }
+// let closureExample = example();
+// console.log(closureExample(100));
+
+// function sayName(name) {
+//   return function massage(text) {
+//     console.log(`${name}: ${text}`);
+//   };
+// }
+// let sayDevid = sayName("Devid");
+// let sayStive = sayName("Stive");
+// sayDevid("Hi");
+// sayStive("Goog morning");
+// sayDevid("Ok");
+
+/////////////////////////////////////////////Practice_start/////////////////////////////////////////////
+
+// 1
+// Используя замыкание написать функцию которая будет вычислять объём параллелепипеда.
+
+// function vParal(h) {
+//   return function (a, b) {
+//     return h * a * b;
+//   };
+// }
+// let showV = vParal(10);
+// console.log(showV(3, 5));
+
+/////////////////////////////////////////////Practice_end/////////////////////////////////////////////
+
+// const user = {
+//   name: "David",
+//   age: 27,
+//   getAge() {
+//     console.log("getAge", this);
+//     console.log(this.age);
+//   },
+//   plusAge() {
+//     this.age += 1;
+//     console.log(this.age);
+//   },
+// };
+
+// const user2 = {
+//   name: "Donna",
+//   age: 40,
+//   plusAge: user.plusAge,
+// };
+
+// user.plusAge();
+// user2.plusAge();
+
+///////////////////////////////////////////////////////////////////////////////////
+
